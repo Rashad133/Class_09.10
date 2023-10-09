1a03fc77d9d666f54dde3682898a100e043bda82
Rashad133 <tu82907hw@code.edu.az>
2023-10-09 05:04:39 +0400

    new task

---
+++ ClassTasks/ClassTasks/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace ClassTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Rashad", "Rufullayev", "AB104", 19);
+
+            student.GetInfo();
+            student.CheckGraduation();
+            student.GetDiplomDegree();
+
+        }
+
+    }
+    class Student
+    {
+        public string Name;
+
+        public string Surname;
+
+        public string Group;
+
+        public byte Point;
+
+        public bool isGraduated;
+
+        public Student(string name, string surname, string group, byte point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Name} {Surname} {Group} {Point} {isGraduated}");
+
+        }
+        public void CheckGraduation()
+        {
+            if (isGraduated)
+            {
+                Console.WriteLine("Telebe mezun ooldu");
+            }
+            else
+            {
+                Console.WriteLine("Telebe mezun olmadi ");
+            }
+        }
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("Diplom ala bilmir");
+            }
+            else if (Point >= 65 && Point <= 80)
+            {
+                Console.WriteLine("Adi diplom");
+            }
+            else if (Point >= 80 && Point <= 90)
+            {
+                Console.WriteLine("Sheref diplomu");
+            }
+            else
+            {
+                Console.WriteLine("Yuksek sheref diplomu");
+            }
+        }
+
+    }
+
+}
---
+++ ClassTasks2(method)/ClassTasks2(method)/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ClassTasks2_method_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Resadinderecesi = 0;
+            int Aydaninderecesi = 0;
+
+            for(int i = 0;i <4;i++)
+            {
+                Console.Write("Resadin xalini daxil edin");
+                int Resadinxali = int .Parse(Console.ReadLine());
+                Console.Write("Aydan xalini daxil edin");
+                int Aydaninxali = int.Parse(Console.ReadLine());
+                int Results = GetCompetition(Resadinxali, Aydaninxali,ref Resadinderecesi, ref Aydaninderecesi);
+
+                Console.WriteLine(Results);
+            }
+        }
+
+
+        public static int GetCompetition(int num1,int num2,ref int num3, ref int num4)
+        {
+            if(num1 > num2)
+            {
+                num3 += 1;
+                return num3;
+
+            }
+
+            else if (num1 < num2)
+            {
+                num4 += 1;
+            }
+            return num4;
+        }
+    }
+}
